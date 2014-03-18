c912e12c2eefe95ab4d679c5135281d80a9d8ade
David Reitz <dreitz@catdevnull.com>
2014-03-17 21:40:12 -0400

    fixed bug where counter was missing on warning/critical lines

--- surfboard6120-snr.py
+++ surfboard6120-snr.py
@@ -54,8 +54,8 @@ smChannels = re.search(r"Channel ID([0-9\s]+)", dOutput).group(1)
 counter = 0
 for current in re.finditer(r"\d+\s", smChannels):
     channel_output = channel_output + "downstreamsnr%d.label Downstream SNR (CHL %s)\n" % (counter, current.group(0))
-    channel_output = channel_output + "downstreamsnr%d.warning 33:\n"
-    channel_output = channel_output + "downstreamsnr%d.critical 30:\n"
+    channel_output = channel_output + "downstreamsnr%d.warning 33:\n" % (counter)
+    channel_output = channel_output + "downstreamsnr%d.critical 30:\n" % (counter)
     counter = counter + 1
 
 # Iterate over Downstream Channels Power
@@ -63,8 +63,8 @@ smChannels = re.search(r"Channel ID([0-9\s]+)", dOutput).group(1)
 counter = 0
 for current in re.finditer(r"\d+\s", smChannels):
     channel_output = channel_output + "downstreampwr%d.label Downstream Power (CHL %s)\n" % (counter, current.group(0))
-    channel_output = channel_output + "downstreampwr%d.warning -12:12\n"
-    channel_output = channel_output + "downstreampwr%d.critical -15:15\n"
+    channel_output = channel_output + "downstreampwr%d.warning -12:12\n" % (counter)
+    channel_output = channel_output + "downstreampwr%d.critical -15:15\n" % (counter)
     counter = counter + 1
 
 # Iterate over Upstream Channels
@@ -72,8 +72,8 @@ smChannels = re.search(r"Channel ID([0-9\s]+)", uOutput).group(1)
 counter = 0
 for current in re.finditer(r"\d+\s", smChannels):
     channel_output = channel_output + "upstreampwr%d.label Upstream Power (CHL %s)\n" % (counter, current.group(0))
-    channel_output = channel_output + "upstreampwr%d.warning 35:52\n"
-    channel_output = channel_output + "upstreampwr%d.critical 8:55\n"
+    channel_output = channel_output + "upstreampwr%d.warning 35:52\n" % (counter)
+    channel_output = channel_output + "upstreampwr%d.critical 8:55\n" % (counter)
     counter = counter + 1
 
 if len(sys.argv) > 1:
